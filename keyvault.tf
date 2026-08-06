e1b7a2f9c1e0a9a52edb8213254d3ebccd9f4f31
NAVNEET SINGH <snavneet36@gmail.com>
2026-08-06 22:47:41 +0530

    Add key vault

---
+++ keyvault.tf
@@ -0,0 +1,15 @@
+resource "azurerm_key_vault" "kv" {
+  name                = "kv-ecommerce-dev001"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  sku_name  = "standard"
+
+  purge_protection_enabled   = false
+  soft_delete_retention_days = 7
+
+  tags = local.common_tags
+}
+
+data "azurerm_client_config" "current" {}
